c4e7caaf6d82ca55411110ded3c453e0281ab1cf
Rizmari Versfeld <rizziepit@gmail.com>
2012-07-25 18:18:36 +0200

    make script generate sql insert for all geo data

--- locations/parse_countries.py
+++ locations/parse_countries.py
@@ -13,18 +13,14 @@ code_geom = {}
 for feature in layer:
     code_geom[feature.get("ISO2")] = {"geom": feature.geom.wkt}
 
-# get full country dataset from foundry
-f = open(os.path.join(base, "../../jmbo-foundry/foundry/fixtures/countries.json"))
+# get full country dataset
+f = open(os.path.join(base, "datasets/countries.json"))
 countries = json.loads(f.read())
 f.close()
+sql_country = "INSERT INTO locations_country (id,name,country_code,border) VALUES ("
 pk = 1
 for country in countries:
     fields = country["fields"]
-    del fields["slug"]
-    del fields["minimum_age"]
-    name = fields["title"]
-    del fields["title"]
-    fields["name"] = name
     code = fields["country_code"]
     fields["border"] = code_geom[code]["geom"] if code in code_geom else None
     if fields["border"] is not None and "MULTIPOLYGON" not in fields["border"]:
@@ -33,77 +29,55 @@ for country in countries:
         code_geom[code]["pk"] = pk
     else:
         code_geom[code] = {"pk": pk}
-    country["model"] = "locations.country"
-    country["pk"] = pk
+    sql_country += "(%d,'%s','%s',ST_GeomFromText('%s',4326))," \
+        % (pk, fields['title'], fields['country_code'], fields['border'])
     pk += 1
-f = open(os.path.join(base, "fixtures/countries.json"), "w")
-f.write(json.dumps(countries))
-f.close()
+sql_country = sql_country[:-1] + ");"
 
 # get region data
 f = open(os.path.join(base, "datasets/MaxMind Cities/fips10-4.csv"))
-regions = []
+sql_region = "INSERT INTO locations_region (id,name,code,country_id) VALUES ("
 pk = 1
 r_code_pk = {}
 for line in f:
     c_code = line[0:2]
     r_code = line[3:5]
     name = line[6:].rstrip().strip('"')
-    regions.append({
-        'pk': pk,
-        'model': 'locations.region',
-        'fields': {
-            'country': code_geom[c_code]["pk"],
-            'code': r_code,
-            'name': name
-        }
-    })
     r_code_pk["%s%s" % (c_code, r_code)] = pk
+    sql_region += "(%d,'%s','%s',%d)," \
+        % (pk, name, r_code, code_geom[c_code]["pk"])
     pk += 1
 f.close()
-f = open(os.path.join(base, "fixtures/regions.json"), "w")
-f.write(json.dumps(regions))
-f.close()
+sql_region = sql_region[:-1] + ");"
 
 # get city data
 f = open(os.path.join(base, "datasets/MaxMind Cities/worldcitiespop.csv"))
+sql_city = "INSERT INTO locations_city (id,name,coordinates,region_id,country_id) VALUES ("
 pk = 1
-city_groups = []
-cities = []
-count = 0
 for line in f:
     els = line.split(",")
     if els[0] != 'Country':
         code = els[0].upper()
-        city = els[2].decode('iso-8859-1').encode('utf8')
+        city = els[2].decode('iso-8859-1')
         lat = els[5]
         lon = els[6].rstrip()
-        city = {
-            "pk": pk,
-            "model": "locations.city",
-            "fields": {
-                "name": city,
-                "country": code_geom[code]["pk"],
-            }
-        }
+        sql_city += "(%d,'%s'," % (pk, city)
         if len(lat) > 0 and len(lon) > 0:
-            city['fields']['coordinates'] = "POINT(%s %s)" % (lon, lat)
-        if els[3] in r_code_pk:
-            city["region"] = r_code_pk["%s%s" % (code, els[3])]
-        cities.append(city)
+            sql_city += "ST_GeomFromText('%s',4326)," % ("POINT(%s %s)" % (lon, lat))
+        else:
+            sql_city += "null,"
+        key = "%s%s" % (code, els[3])
+        if key in r_code_pk:
+            sql_city += "%d," % r_code_pk[key]
+        else:
+            sql_city += "null,"
+        sql_city += "%d)," % (code_geom[code]["pk"], )
         pk += 1
-        count += 1
-        if count >= 100000:
-            count = 0
-            city_groups.append(cities)
-            cities = []
-city_groups.append(cities)    
+        print(pk)
 f.close()
-count = 1
-for cities in city_groups:
-    f = open(os.path.join(base, "fixtures/cities_%d.json" % count), "w")
-    f.write(json.dumps(cities))
-    f.close()
-    count += 1
+sql_city = sql_city[:-1] + ");"
 
-
+f = open(os.path.join(base, "datasets/data.sql"), 'w')
+sql = sql_country + sql_region + sql_city
+f.write(sql.encode('utf8'))
+f.close()
