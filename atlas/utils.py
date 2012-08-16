03344a4ae9d10b6fb8a8e717d9efd937d69bf351
Rizmari Versfeld <rizziepit@gmail.com>
2012-08-16 13:37:02 +0200

    city lookup now acceptably fast on mysql - for a US city, 200ms if IP and position is known, 350ms if just position is known

--- atlas/utils.py
+++ atlas/utils.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 import math
 
 from django.contrib.gis.geoip import GeoIP
 from django.contrib.gis.geos import fromstr
+from django.contrib.gis.measure import D
 from django.conf import settings
 from django.db.models.query import Q
 
@@ -17,6 +19,7 @@ An IP address lookup is fairly fast while a position lookup is expensive.
 '''
 def get_city(ip=None, position=None):
     city = None
+    city_geoip = None
     qs = None
     
     # narrow location down to a country using IP address
@@ -35,7 +38,7 @@ def get_city(ip=None, position=None):
     # find closest city based on GPS coordinates
     if position:
         # if we don't have a country-filtered queryset (no IP address or IP lookup failed)
-        if not qs:
+        if not city_geoip:
             if DB_ENGINE.rfind('mysql') >= 0:
                 # select 5 closest countries
                 closest_countries = Country.objects.exclude(coordinates__isnull=True).extra(select={'distance': \
@@ -69,17 +72,29 @@ def get_city(ip=None, position=None):
             closest_regions = [r.id for r in regions]
 
             qs = City.objects.filter(Q(region__in=closest_regions) | Q(country__in=closest_countries))
-        
-        # select cities within a minimum bounding rectangle of 100km radius
-        #qs = qs.filter(coordinates__within=)
-        
-        # order cities by their distance
-        if DB_ENGINE.rfind('mysql') >= 0:
-            qs = qs.extra(select={'distance': \
-                'distance_sphere(`atlas_city`.`coordinates`, geomfromtext(\'%s\', %d))' \
-                % (str(position), position.srid)}).order_by('distance')
+
+        # try to select only closest regions if we know the country
         else:
-            qs = qs.distance(position).order_by('distance')
+            # assumes I either have centroid coordinates for all regions in a country, or none
+            closest_regions = Region.objects.exclude(coordinates__isnull=True).filter( \
+                country__country_code=city_geoip['country_code'])
+            if DB_ENGINE.rfind('mysql') >= 0:
+                closest_regions = closest_regions.extra(select={'distance': \
+                    'distance_sphere(`atlas_region`.`coordinates`, geomfromtext(\'%s\', %d))' \
+                    % (str(position), position.srid)}).order_by('distance')[:5]
+            else:
+                closest_regions = closest_regions.distance(position).order_by('distance')[:5]
+            if closest_regions:
+                closest_regions = [r.id for r in closest_regions]
+                qs = qs.filter(region__in=closest_regions)
+        
+        # select cities within a minimum bounding rectangle of 100km radius and order cities by their distance
+        if DB_ENGINE.rfind('mysql') >= 0:
+            qs = qs.filter(coordinates__within=get_mbr_for_radius(position, 100000)).extra( \
+                select={'distance': '''distance_sphere(`atlas_city`.`coordinates`, 
+                geomfromtext(\'%s\', %d))''' % (str(position), position.srid)}).order_by('distance')
+        else:
+            qs = qs.filter(coordinates__dwithin=(position, D(m=100000))).distance(position).order_by('distance')
         
         # select closest city
         if qs:
@@ -88,20 +103,32 @@ def get_city(ip=None, position=None):
     return city
 
 
+'''
+Returns a polygon that encloses a circle of the specified radius, centered at the
+specified point. It takes into account the decrease in spacing between longitude lines
+towards the poles.
+'''
 def get_mbr_for_radius(point, radius):
-    theta = radius / RADIUS_EARTH
-    theta_deg = theta * 180 / math.pi
+    theta = float(radius) / RADIUS_EARTH * 180 / math.pi
 
-    min_lat = point.y - theta_deg
+    min_lat = point.y - theta
     if min_lat < -90:
         min_lat = -180 - min_lat
     
-    max_lat = point.y + theta_deg
+    max_lat = point.y + theta
     if max_lat > 90:
         max_lat = 180 - max_lat
     
+    # adjacent = cosX * hypotenuse
+    theta_at_min_lat = radius / (math.cos(min_lat / 180 * math.pi) * RADIUS_EARTH) * 180 / math.pi
+    theta_at_max_lat = radius / (math.cos(max_lat / 180 * math.pi) * RADIUS_EARTH) * 180 / math.pi
     
-
+    max_lon_at_min_lat = (point.x + theta_at_min_lat + 180) % 360 - 180
+    min_lon_at_min_lat = (point.x - theta_at_min_lat + 180) % 360 - 180
+    max_lon_at_max_lat = (point.x + theta_at_max_lat + 180) % 360 - 180
+    min_lon_at_max_lat = (point.x - theta_at_max_lat + 180) % 360 - 180
+    
     return fromstr("POLYGON((%f %f, %f %f, %f %f, %f %f, %f %f))" % \
-        (min_lon, min_lat, min_lon, max_lat, max_lon, max_lat, max_lon, min_lat, min_lon, min_lat), \
-        point.srid)
+        (min_lon_at_min_lat, min_lat, max_lon_at_min_lat, min_lat, \
+        max_lon_at_max_lat, max_lat, min_lon_at_max_lat, max_lat, \
+        min_lon_at_min_lat, min_lat), srid=point.srid)
